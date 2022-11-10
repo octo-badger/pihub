017e4bb38f4ac72ceb94e42de765dc05220c5091
richard <lidstone@gmail.com>
2022-11-10 19:26:24 +1100

    swapping out DynamicEase for StatusValue

--- index.js
+++ index.js
@@ -39,12 +39,11 @@ const server = require('./server');
 const socketio = require('socket.io');
 const io = socketio(server);
 
-//const Gpio = require('pigpio').Gpio;
 const pigpio = require('pigpio');
 const Gpio = pigpio.Gpio;
 
 const sunstatus = require('./sunStatus')
-const DynamicEase = require('./lib/DynamicEase');
+const StatusValue = require('./lib/StatusValue');
 const Settings = require('./lib/Settings');
 
 const settings = new Settings(
@@ -87,9 +86,13 @@ async function go()
 
     let pins = config.pins;
 
+    /*
     const doorLightR = new Gpio(pins.doorLightR, { mode: Gpio.OUTPUT });
     const doorLightG = new Gpio(pins.doorLightG, { mode: Gpio.OUTPUT });
     const doorLightB = new Gpio(pins.doorLightB, { mode: Gpio.OUTPUT });
+    /*/
+    const [doorLightR, doorLightG, doorLightB] = ['R','G','B'].map(c => new Gpio(pins['doorLight'+c], { mode: Gpio.OUTPUT }));
+    //*/
 
     //let start = Date.now();
 
@@ -98,57 +101,61 @@ async function go()
     let grnOptions = merge(config.lightOptions, config.green);
     let bluOptions = merge(config.lightOptions, config.blue);
 
-    let easeB = new DynamicEase(0, bluOptions,
+    let easeB = new StatusValue(0, bluOptions,
                     {
                         onUpdate: pwm => 
                         {   
                             //console.log(`${Date.now() - start}, ${pwm}`);
                             doorLightB.pwmWrite(pwm);
-                        },
-                        onComplete: ease => 
-                        {
-                            //console.log(`completed: ${ease.target}`);
-                            ease.set(ease.target === 0 ? 128 : 0);
                         }
                     });
 
-    // let easeG = new DynamicEase(0, grnOptions,
-    //     {
-    //         onUpdate: pwm => 
-    //         {   
-    //             //console.log(`${Date.now() - start}, ${pwm}`);
-    //             doorLightB.pwmWrite(pwm);
-    //         },
-    //         onComplete: ease => 
-    //         {
-    //             //console.log(`completed: ${ease.target}`);
-    //             ease.set(ease.target === 0 ? 128 : 0);
-    //         }
-    //     });
+    let easeG = new StatusValue(0, grnOptions,
+                    {
+                        onUpdate: pwm => 
+                        {   
+                            //console.log(`${Date.now() - start}, ${pwm}`);
+                            doorLightG.pwmWrite(pwm);
+                        }
+                    });
+    
+
+    let easeR = new StatusValue(0, redOptions,
+        {
+            onUpdate: pwm => doorLightR.pwmWrite(pwm),
+        });
+
+
+    let updateState = () =>
+    {
+        console.debug(`setting door closed: ${isDoorClosed}`);
+        //easeR.set(isDoorClosed ? 0 : 255);
+        isDoorClosed ? easeR.pause(0) : easeR.resume();
+
+        console.debug(`setting connected: ${isConnected}`);
+        //easeG.set(isConnected ? 0 : 255);
+        isConnected ? easeG.pause(0) : easeG.resume();
+    };
 
 
     io.on('connection', (socket) =>
     {
         console.log('websocket connection');
         isConnected = true;
+        updateState();
 
         socket.on("disconnecting", (reason) => 
         {
             console.log(`disconnected: ${reason}`);
             isDoorClosed = isConnected = false;
+            updateState();
         }); 
 
-        let easeR = new DynamicEase(0, redOptions,
-                        {
-                            onUpdate: pwm => doorLightR.pwmWrite(pwm),
-                            onComplete: null //() => console.log(`completed`)
-                        });
-
-        socket.on('test', (msg) => 
+        socket.on('door', (msg) => 
         {
-            console.log(`test: ${msg}`);
-            isDoorClosed = (msg === 'door closed');
-            easeR.set(isDoorClosed ? 0 : 255);
+            console.log(`door ${msg}`);
+            isDoorClosed = (msg === 'closed');
+            updateState();
         });
     });
 
